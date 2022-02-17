6f2d4f7d01c8b43df3585d1d9c5ee03c96375297
sazzad <hossensazzad1201@gmail.com>
2022-02-17 23:11:16 +0600

    website update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
+// get value 
+function getValue(need) {
+    let inputField = document.getElementById(need + "-input").value
+    return inputField;
+}
+// calculate button 
 document.getElementById("calculate-btn").addEventListener("click", function () {
-    let incomeField = document.getElementById("income-input")
-    let incomeValue = incomeField.value;
-
-    let foodField = document.getElementById("food-input").value
-    let rentField = document.getElementById("rent-input").value
-    let clothesField = document.getElementById("clothes-input").value
-
-    if (isNaN(incomeValue) || isNaN(foodField) || isNaN(rentField) || isNaN(clothesField)) {
+    let incomeField = getValue("income")
+    let foodField = getValue("food")
+    let rentField = getValue("rent")
+    let clothesField = getValue("clothes")
+    if (isNaN(incomeField) || isNaN(foodField) || isNaN(rentField) || isNaN(clothesField)) {
         let failError = document.getElementById("notify-fail")
         failError.style.display = "block"
         return
     }
-    if (incomeValue < 0 || foodField < 0 || rentField < 0 || clothesField < 0) {
+    if (incomeField < 0 || foodField < 0 || rentField < 0 || clothesField < 0) {
         let fialError = document.getElementById("notify-fail")
         fialError.style.display = "block"
         return
     }
     else {
         let inputExpensesField = parseInt(foodField) + parseInt(rentField) + parseInt(clothesField)
-        if (inputExpensesField > incomeValue) {
+        if (inputExpensesField > incomeField) {
             let failErrorTwo = document.getElementById("notify-fail-2")
             failErrorTwo.style.display = "block"
             return
@@ -27,36 +30,44 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
         totalExpensesField.innerText = inputExpensesField
 
         let balanceField = document.getElementById("balance")
-        balanceField.innerText = parseInt(incomeValue) - inputExpensesField
+        balanceField.innerText = parseInt(incomeField) - inputExpensesField
         saveMoney()
     }
 })
-
+// save  button 
 function saveMoney() {
     document.getElementById("save-btn").addEventListener("click", function () {
+        let incomeField = getValue("income")
+        let foodField = getValue("food")
+        let rentField = getValue("rent")
+        let clothesField = getValue("clothes")
+
         let saveInput = document.getElementById("save-input")
         let saveValue = saveInput.value
 
-        let incomeField = document.getElementById("income-input")
-        let incomeValue = incomeField.value;
-        let savePercentage = (incomeValue * saveValue) / 100
-        document.getElementById("saving-amount").innerText = savePercentage
+        let savePercentage = (incomeField * saveValue) / 100
+        let inputExpensesField = parseInt(foodField) + parseInt(rentField) + parseInt(clothesField)
+
+        let balanceField = parseInt(incomeField) - inputExpensesField
+        if (savePercentage < balanceField) {
+            document.getElementById("saving-amount").innerText = savePercentage
+        }
         remainingMoney()
     })
 }
+// remaining balance 
 function remainingMoney() {
-    let incomeField = document.getElementById("income-input")
-    let incomeValue = incomeField.value;
+    let incomeField = getValue("income")
+    let foodField = getValue("food")
+    let rentField = getValue("rent")
+    let clothesField = getValue("clothes")
 
-    let foodField = document.getElementById("food-input").value
-    let rentField = document.getElementById("rent-input").value
-    let clothesField = document.getElementById("clothes-input").value
     let inputExpensesField = parseInt(foodField) + parseInt(rentField) + parseInt(clothesField)
 
-    let balanceField = parseInt(incomeValue) - inputExpensesField
+    let balanceField = parseInt(incomeField) - inputExpensesField
     let saveInput = document.getElementById("save-input")
     let saveValue = saveInput.value
-    let savePercentage = (incomeValue * saveValue) / 100
+    let savePercentage = (incomeField * saveValue) / 100
     if (balanceField < savePercentage) {
         let failErrorTwo = document.getElementById("notify-fail-2")
         failErrorTwo.style.display = "block"
